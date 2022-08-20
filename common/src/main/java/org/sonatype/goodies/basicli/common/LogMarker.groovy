fb2174bfcc2b2a6544c4fb88a0ddd4f76cfa75ae
Jason Dillon <jdillon@users.noreply.github.com>
2022-08-20 14:04:48 -0700

    A bit more copy-pasta

---
+++ common/src/main/java/org/sonatype/goodies/basicli/common/LogMarker.groovy
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.goodies.basicli.common
+
+import groovy.transform.NullCheck
+import org.slf4j.Logger
+
+/**
+ * Helper to log a block of text in a marked section of the log.
+ *
+ * @since ???
+ */
+class LogMarker
+{
+  private LogMarker() {
+    // empty
+  }
+
+  @NullCheck
+  static void mark(final Logger logger, Level level, final String prefix, final String text) {
+    level.log(logger, "$prefix\n----8<----\n${text}\n---->8----")
+  }
+}
